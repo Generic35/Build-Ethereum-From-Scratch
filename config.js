43c4660b3037b0fd7f88e4bbfcb8b8dad778beb9
Eric Lamontagne <ricklamontagne@gmail.com>
2021-10-31 17:53:49 -0400

    feat: Add blockchain and block classes

---
+++ config.js
@@ -0,0 +1,14 @@
+const GENESIS_DATA = {
+	blockHeaders: {
+		parentHash: '--genesis-parent-hash--',
+		beneficierary: '--genesis-beneficiary--',
+		difficulty: 1,
+		number: 0,
+		timestap: '--genesis-timestamp--',
+		nonce: 0
+	}
+}
+
+module.exports = {
+	GENESIS_DATA
+}
