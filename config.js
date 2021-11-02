1a0bd215a292a95569c4c3cce6ad6cb6f292f320
Eric Lamontagne <ricklamontagne@gmail.com>
2021-11-02 15:05:03 -0400

    feat: Add the ability to validate a block before adding it to the chain

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const GENESIS_DATA = {
 	blockHeaders: {
 		parentHash: '--genesis-parent-hash--',
 		beneficierary: '--genesis-beneficiary--',
-		difficulty: 100000,
+		difficulty: 1,
 		number: 0,
 		timestamp: '--genesis-timestamp--',
 		nonce: 0
