7a2e6a32b2d48f149b308f743454e4d4a32f2ca1
Eric Lamontagne <ricklamontagne@gmail.com>
2021-11-02 12:59:58 -0400

    feat: Add the ability to mine until the correct nonce is found

--- blockchain/block.js
+++ blockchain/block.js
@@ -1,8 +1,9 @@
 const { GENESIS_DATA } = require('../config');
+const { keccakHash } = require('../util');
 
 const HASH_LENGTH = 64;
 const MAX_HASH_VALUE = parseInt('f'.repeat(HASH_LENGTH), 16);
-
+const MAX_NONCE_VALUE = 2 ** 64;
 class Block {
 	constructor({ blockHeaders }){
 		this.blockHeaders = blockHeaders;
@@ -17,9 +18,36 @@ class Block {
 
 		return '0'.repeat(HASH_LENGTH - value.length) + value;
 	}
-
+	
 	static mineBlock({ lastBlock, beneficiary }) {
-		
+		const target = Block.calculateBlockTargetHash({ lastBlock })
+		let timestamp, truncatedBlockHeaders, header, nonce, underTargetHash
+
+		do {
+			timestamp = Date.now();
+			truncatedBlockHeaders = {
+				parentHash: keccakHash(lastBlock.blockHeaders),
+				beneficiary,
+				difficulty: lastBlock.blockHeaders.difficulty + 1,
+				number: lastBlock.blockHeaders.number + 1,
+				timestamp
+			}
+			header = keccakHash(truncatedBlockHeaders);
+			nonce = Math.floor(Math.random() * MAX_NONCE_VALUE);
+	
+			underTargetHash = keccakHash(header + nonce);
+		} while (underTargetHash > target);
+
+	
+
+		console.log('underTargetHash ', underTargetHash)
+		console.log('target ', target)
+
+		return new this({
+			blockHeaders: {
+				...truncatedBlockHeaders, nonce }
+			
+		})
 	}
 
 	static genesis() {
@@ -28,3 +56,10 @@ class Block {
 }
 
 module.exports = Block;
+
+const block = Block.mineBlock({
+	lastBlock: Block.genesis(),
+	beneficiary: 'foo'
+})
+
+console.log('block', block);
