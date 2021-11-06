27801b2269880c553cd929a611df7c18b3b02b39
Eric Lamontagne <ricklamontagne@gmail.com>
2021-11-06 18:47:33 -0400

    feat: Add an endpoint allowing an http post to create a transction

---
+++ api-test.js
@@ -0,0 +1,38 @@
+const { post } = require('request');
+const request = require('request');
+
+const BASE_URL = 'http://localhost:3000';
+
+const postTransact = ({ to, value }) => {
+  return new Promise((resolve, reject) => {
+    request(
+      `${BASE_URL}/account/transact`,
+      {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ to, value }),
+      },
+      (error, response, body) => {
+        return resolve(JSON.parse(body));
+      }
+    );
+  });
+};
+
+postTransact({})
+  .then((postTransactionResponse) => {
+    console.log(
+      'postTransctResponse (Create Account Transaction): ',
+      postTransactionResponse
+    );
+
+    const toAccountData = postTransactionResponse.transaction.data.accountData;
+
+    return postTransact({ to: toAccountData.address, value: 20 });
+  })
+  .then((postTransactionResponse2) => {
+    console.log(
+      'postTransctionResponse2( Standard Transaction)',
+      postTransactionResponse2
+    );
+  });
