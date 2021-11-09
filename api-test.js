5dcb0268e67da8d3b8ea2450dd395e02e1f18bbf
Eric Lamontagne <ricklamontagne@gmail.com>
2021-11-09 14:54:00 -0500

    feat: Add the ability to get the balance of the account of the miner running the app, and the balance for any account

--- api-test.js
+++ api-test.js
@@ -29,6 +29,17 @@ const getMine = () => {
   });
 };
 
+const getAccountBalance = ({ address } = {}) => {
+  return new Promise((resolve, reject) => {
+    request.get(
+      `${BASE_URL}/account/balance` + (address ? `?address=${address}` : ''),
+      (error, response, body) => {
+        return resolve(JSON.parse(body));
+      }
+    );
+  });
+};
+
 let toAccountData;
 
 postTransact({})
@@ -57,4 +68,13 @@ postTransact({})
   })
   .then((getMineResponse2) => {
     console.log('getMineResponse2 ', getMineResponse2);
+    return getAccountBalance();
+  })
+  .then((getAccountBalanceResponse) => {
+    console.log('getAccountBalanceResponse ', getAccountBalanceResponse);
+
+    return getAccountBalance({ address: toAccountData.address });
+  })
+  .then((getAccountBalanceResponse2) => {
+    console.log('getAccountBalanceResponse2 :', getAccountBalanceResponse2);
   });
